111ab05087ba2239378ec77b09db38edd133b1ff
Tomek Wilczynski <106587849+Wilkubone@users.noreply.github.com>
2024-10-23 19:58:04 +0200

    email validation...add new toaster...other changes

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,46 @@
 "use server";
 
+import React from "react";
+import { getErrorMessage, validateString } from "@/lib/utils";
+import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
-	console.log("Sending email");
-	console.log(formData.get("senderEmail"));
-	console.log(formData.get("message"));
+	const senderEmail = formData.get("senderEmail");
+	const message = formData.get("message");
+
+	if (!validateString(senderEmail, 500)) {
+		return {
+			error: "Invalid sender email",
+		};
+	}
+	if (!validateString(message, 5000)) {
+		return {
+			error: "Invalid message",
+		};
+	}
+
+	let data;
+	try {
+		data = await resend.emails.send({
+			from: "Contact Form <onboarding@resend.dev>",
+			to: "wilkubone@googlemail.com",
+			subject: "New message from your form",
+			replyTo: senderEmail as string,
+			// text: message as string,
+			react: React.createElement(ContactFormEmail, {
+				message: message as string,
+				senderEmail: senderEmail as string,
+			}),
+		});
+	} catch (error) {
+		return {
+			error: getErrorMessage(error),
+		};
+	}
+	return {
+		data,
+	};
 };
--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import SectionHeading from './section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { useSectionInView } from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
+import toast from 'react-hot-toast';
+import SubmitBtn from './submit-btn';
 
 const Contact = () => {
 const {ref} = useSectionInView("Contact")
@@ -25,12 +26,18 @@ const {ref} = useSectionInView("Contact")
 
         <form className='mt-10 flex flex-col'
         action={async (formData) => {
-            await sendEmail(formData);
+            const { data, error } = await sendEmail(formData);
+
+            if (error) {
+              toast.error(error);
+              return;
+            }
+            toast.success('Email sent successfully!')
         }}
         >
             <input className='h-14 px-4 rounded-lg borderBlack' name='senderEmail' type="email" required maxLength={500} placeholder='Your email'/>
-            <textarea className='h-52 my-3 rounded-lg borderBlack p-4' name="message" placeholder='Your message' required maxLength={500}/>
-            <button type='submit' className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105'>Submit <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/> </button>
+            <textarea className='h-52 my-3 rounded-lg borderBlack p-4' name="message" placeholder='Your message' required maxLength={5000}/>
+            <SubmitBtn/>
         </form>
     </motion.section>
   )
